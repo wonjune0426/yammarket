3a338aaf8a2c0c433433ddbfe8eedb3b00356bc8
chw_owo <gpdnjs2116@naver.com>
2022-02-12 14:41:04 +0900

    settings

---
+++ src/main/java/com/example/yammarket/YammarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.yammarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YammarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YammarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yammarket/YammarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yammarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YammarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
