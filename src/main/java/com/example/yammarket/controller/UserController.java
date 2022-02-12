cbf0cb48d44abbf773563ad98701cf9c6f891fc8
chw_owo <gpdnjs2116@naver.com>
2022-02-12 21:17:44 +0900

    syntax error 해결

--- src/main/java/com/example/yammarket/controller/UserController.java
+++ src/main/java/com/example/yammarket/controller/UserController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class UserController {
 
     private final UserService userService;
