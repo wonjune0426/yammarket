6369bec562194fd08b8e499648d25a31bbc557e8
wonjune0426 <wonjune0426@naver.com>
2022-02-14 17:41:15 +0900

    commit test

---
+++ src/main/java/com/example/yammarket/model/Test.java
@@ -0,0 +1,17 @@
+package com.example.yammarket.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Test {
+    private String msg;
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "msg='" + msg + '\'' +
+                '}';
+    }
+}
