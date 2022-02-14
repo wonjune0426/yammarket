802bd99691a9c22b1f7de2f775b7f1658fbeadb8
wonjune0426 <wonjune0426@naver.com>
2022-02-14 15:08:19 +0900

    간단한 수정

--- src/main/java/com/example/yammarket/service/BookmarkService.java
+++ src/main/java/com/example/yammarket/service/BookmarkService.java
@@ -5,12 +5,15 @@ import com.example.yammarket.model.Posts;
 import com.example.yammarket.model.Users;
 import com.example.yammarket.repository.BookmarkRepository;
 import com.example.yammarket.repository.PostRepository;
+import com.example.yammarket.repository.UserRepository;
 import com.example.yammarket.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public boolean registerBookmarks(@PathVariable Long postId,
                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
