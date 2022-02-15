825c20a79cc52cfec32d2c64bc215b1365a9b3db
wonjune0426 <wonjune0426@naver.com>
2022-02-15 21:38:46 +0900

    @CrossOrigin 수정

--- src/main/java/com/example/yammarket/controller/BookmarkController.java
+++ src/main/java/com/example/yammarket/controller/BookmarkController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
